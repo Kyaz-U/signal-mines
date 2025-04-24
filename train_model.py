aacf68edd9fda4632e44f86dd46a79c06650acee
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-25 01:34:19 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,28 +6,22 @@ import os
 model_path = "models/mines_rf_models.pkl"
 csv_path = "data/mines_data.csv"
 
-# CSV fayl borligini tekshiramiz
 if not os.path.exists(csv_path):
-    print("❌ data/mines_data.csv topilmadi. Iltimos, /bombs orqali ma’lumot kiriting.")
+    print("❌ data/mines_data.csv topilmadi. Iltimos, /bombs orqali ma'lumot kiriting.")
     exit()
 
-# CSVni o‘qiymiz
 df = pd.read_csv(csv_path)
-
-# bomb_count ustunini chiqaramiz
 X = df.drop(columns=["bombs_count"])
-models = {}
 
-# Har bir katak uchun alohida model
-for i in range(25):
-    col = f"cell_{i+1}"
+models = {}
+for i in range(1, 26):
+    col = f"cell_{i}"
     y = X[col]
     X_temp = X.drop(columns=[col])
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_temp, y)
     models[col] = model
 
-# Modellarni saqlaymiz
 os.makedirs("models", exist_ok=True)
 joblib.dump(models, model_path)
 print("✅ Model saqlandi:", model_path)
