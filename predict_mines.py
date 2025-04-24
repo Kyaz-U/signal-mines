9b726cb89d3f300e9510ed0a43c46876126a3d38
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-24 18:20:39 +0500

    Update predict_mines.py

--- predict_mines.py
+++ predict_mines.py
@@ -12,21 +12,24 @@ models = joblib.load("models/mines_rf_models.pkl")
 
 # Eng xavfsiz kataklarni aniqlovchi funksiyani yozamiz
 def predict_safest_cells(latest_games_df, top_k=6):
-    if latest_games_df.shape[0] <= 5:
-        print("Kamida 5 ta o‘yinga ehtiyoj bor.")
+    if latest_games_df.shape[0] < 1:
+        print("Kamida 1 ta o‘yinga ehtiyoj bor.")
         return []
 
-    df_to_use = latest_games_df.copy()
+    df_to_use = latest_games_df.tail(1).copy()
+
     if 'cell25' in df_to_use.columns:
         df_to_use = df_to_use.drop(columns=["cell25"])
 
-    avg_row = df_to_use.mean().values.reshape(1, -1)
+    avg_row = df_to_use.values.reshape(1, -1)
 
     predictions = {}
     for i in range(25):
-        model = models[f"cell_{i+1}"]
-        prob = model.predict_proba(avg_row)[0][1]
-        predictions[f"cell_{i+1}"] = prob
+        key = f"cell_{i+1}"
+        if key in models:
+            model = models[key]
+            prob = model.predict_proba(avg_row)[0][1]
+            predictions[key] = prob
 
     safest = sorted(predictions.items(), key=lambda x: x[1], reverse=True)[:top_k]
     return [cell for cell, prob in safest]
