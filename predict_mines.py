d602779acd6dd1b93f52dc59778d04fd5e60af03
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-25 01:36:17 +0500

    Update predict_mines.py

--- predict_mines.py
+++ predict_mines.py
@@ -4,6 +4,7 @@ import os
 import matplotlib.pyplot as plt
 
 model_path = "models/mines_rf_models.pkl"
+
 if not os.path.exists(model_path):
     os.system("python train_model.py")
 
@@ -16,9 +17,10 @@ def predict_safest_cells(data, top_k=6):
         avg_row = data.tail(5).mean().values.reshape(1, -1)
 
     predictions = {}
-    for i in range(25):
-        col = f"cell_{i+1}"
-        prob = models[col].predict_proba(avg_row)[0][1]  # xavfsizlik ehtimoli
+    for i in range(1, 26):
+        col = f"cell_{i}"
+        X_input = pd.DataFrame(avg_row).drop(columns=[i-1], axis=1)
+        prob = models[col].predict_proba(X_input)[0][0]  # xavfsizlik ehtimoli
         predictions[col] = prob
 
     safest = sorted(predictions.items(), key=lambda x: x[1], reverse=True)[:top_k]
@@ -28,7 +30,6 @@ def predict_safest_cells(data, top_k=6):
 def draw_chart(predictions):
     keys = list(predictions.keys())
     vals = [v * 100 for v in predictions.values()]
-
     plt.figure(figsize=(10, 4))
     plt.bar(keys, vals)
     plt.xticks(rotation=90)
