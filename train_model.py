b3966fa6e475ea514f1ef5c904d21a32933aa787
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-24 23:44:18 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,26 +6,19 @@ import os
 model_path = "models/mines_rf_models.pkl"
 csv_path = "data/mines_data.csv"
 
-# CSV fayl borligini tekshiramiz
 if not os.path.exists(csv_path):
-    print("❌ data/mines_data.csv topilmadi. Iltimos, avval /bombs orqali ma'lumot kiriting.")
-    exit()
+    raise FileNotFoundError("CSV topilmadi, iltimos, /bombs orqali ma'lumot kiriting.")
 
-# CSV ni o‘qiymiz
 df = pd.read_csv(csv_path)
-X = df.drop(columns=["bombs_count"]) if "bombs_count" in df.columns else df.copy()
-
-# Modelni yaratamiz
+X = df.drop(columns=["bombs_count"])
 models = {}
 for i in range(25):
     col = f"cell_{i+1}"
     y = df[col]
     X_temp = X.drop(columns=[col])
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
     model.fit(X_temp, y)
     models[col] = model
 
-# Modelni saqlaymiz
 os.makedirs("models", exist_ok=True)
 joblib.dump(models, model_path)
-print(f"✅ Model(lar) yangilandi va '{model_path}' faylga saqlandi.")
