831c2cc95e5b8274f43382d435566387a67c19e8
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-25 18:23:20 +0500

    Update logger.py

--- modules/logger.py
+++ modules/logger.py
@@ -1,7 +1,13 @@
+import logging
 
-import datetime
+logging.basicConfig(
+    filename='log.txt',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-def log_event(message):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("log.txt", "a") as f:
-        f.write(f"[{timestamp}] {message}\n")
+def log_info(message):
+    logging.info(message)
+
+def log_error(message):
+    logging.error(message)
