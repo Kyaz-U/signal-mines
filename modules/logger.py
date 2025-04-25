3b8bcce19603ef8c520aae051ee36fd59af4d950
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-25 20:17:47 +0500

    Update logger.py

--- modules/logger.py
+++ modules/logger.py
@@ -1,13 +1,12 @@
 import logging
+from datetime import datetime
 
+# Logger sozlamasi
 logging.basicConfig(
-    filename='log.txt',
+    filename="log.txt",
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-def log_info(message):
+def log_event(message):
     logging.info(message)
-
-def log_error(message):
-    logging.error(message)
